b51c086d049ffef1500ccfef0486a38a28d3662d
khatia-turm <turmanidzekhatia66@gmail.com>
2025-09-05 21:41:18 +0400

    saved

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
 "use strict";
 
+// LEARN MORE SMOOTH SCROLL
+const btnScroll = document.querySelector(".btn--scroll");
+const section1 = document.getElementById("section--1");
+
+btnScroll.addEventListener("click", function (e) {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+//PAGE NAVIGATION SMOOTH
+
+document.querySelector(".nav-links").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(e.target);
+  if (e.target.classList.contains("nav_link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// TAB COMPONENET
+
+const tabsContainer = document.querySelector(".features__tab-container");
+const featuresContent = document.querySelectorAll(".features__content");
+const tabs = document.querySelectorAll(".features__tab");
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".features__tab");
+  if (!clicked) return;
+  tabs.forEach((t) => t.classList.remove("features__tab--active"));
+  featuresContent.forEach((tc) =>
+    tc.classList.remove("features__content--active")
+  );
+  clicked.classList.add("features__tab--active");
+  document
+    .querySelector(`.features__content--${clicked.dataset.tab}`)
+    .classList.add("features__content--active");
+  // console.log(
+  //   document.querySelector(`features__tab--${e.target.dataset.tab}`)
+  // );
+});
+
+// tabsContainer.addEventListener("click", function (e) {
+//   if (e.target.classList.contains("features__tab")) {
+//     console.log("clicked");
+//     tabs.forEach((t) => t.classList.remove("features__tab--active"));
+//     featuresContent.forEach((tc) =>
+//       tc.classList.remove("features__content--active")
+//     );
+//     e.target.classList.add("features__tab--active");
+//     document
+//       .querySelector(`.features__content--${e.target.dataset.tab}`)
+//       .classList.add("features__content--active");
+//   }
+// });
+
+// opening modal
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
