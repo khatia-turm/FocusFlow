dbd91e0d24a97ed1ce42846fdf79eb4816727e93
khatia-turm <turmanidzekhatia66@gmail.com>
2025-09-06 20:58:50 +0400

    saved

--- BE/be.php
+++ BE/be.php
@@ -58,10 +58,10 @@ if (isset($_FILES['upload']) && $_FILES['upload']['error'] === UPLOAD_ERR_OK) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Quiz Results - Study Hub</title>
+  <title>Quiz Results - FocusFlow</title>
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@400;500&display=swap" rel="stylesheet">
-  <link href="css/results.css" rel="stylesheet">
-  <link rel="icon" href="/images/Untitled.PNG" type="image/icon">
+  <link href="/css/results.css" rel="stylesheet">
+  <link rel="icon" href="/img/logo.png" type="image/icon">
 </head>
 
 <body class="result-body">
